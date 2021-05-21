fb4d4d37eea5546e2c426d7851bf9cdb8cd34346
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2021-05-21 09:07:51 +0200

    suite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {
-  }
+    public firstname = '';
+    public lastname = '';
+
+    constructor() {
+    }
 
 
+    public addNewDriver(firstname: string, lastname: string) {
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {TableModule} from 'primeng/table';
 import {ButtonModule} from 'primeng/button';
 import {SharedModule} from 'primeng/api';
 import {InputTextModule} from 'primeng/inputtext';
 import {FormsModule} from '@angular/forms';
+import {Route, RouterModule} from '@angular/router';
+import {StaffVisualizerComponent} from './staff-list/staff-visualizer/staff-visualizer.component';
+
+
+const routes: Route[] = [
+    {path: '', component: StaffVisualizerComponent}
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TableModule,
-    ButtonModule,
-    SharedModule,
-    InputTextModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        AppRoutingModule,
+        TableModule,
+        ButtonModule,
+        SharedModule,
+        InputTextModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
