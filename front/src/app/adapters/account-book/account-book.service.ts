605ec673abddf280ad18aa4b13ab9528f33124b7
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2021-05-31 00:52:13 +0200

    adding socket.io and types for back and front

--- front/src/app/adapters/account-book/account-book.service.ts
+++ front/src/app/adapters/account-book/account-book.service.ts
@@ -2,17 +2,28 @@ import {Injectable, InjectionToken} from '@angular/core';
 import AccountBook from '../../domain/ports/account-book';
 import Taxi from '../../domain/model/taxi/taxi';
 import Client from '../../domain/model/client/client';
+import * as io from 'socket.io-client';
+import Socket = SocketIOClient.Socket;
 
 export let ACCOUNT_BOOK = new InjectionToken<AccountBook>('AccountBook');
 
 @Injectable()
 export class AccountBookService implements AccountBook {
 
-    constructor() {
-    }
+    private _socket: Socket = io('http://localhost:3000/');
 
     public registerClientBill(taxi: Taxi, client: Client): void {
-        console.log('RegisterClientBill');
+
+        this._socket.on('events', function (data: any) {
+            console.log('event', data);
+        });
+        this._socket.on('exception', (data: any) => {
+            console.log('event', data);
+        });
+        this._socket.on('disconnect', function () {
+            console.log('Disconnected');
+        });
+        this._socket.emit('events', {test: 'test'});
     }
 
     public registerDrive(taxi: Taxi): void {
